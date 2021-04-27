45c0f3e9c986b1e05eae982c8055b9045fce925f
git4mahadi <mahadi.diu791@gmail.com>
2021-04-27 22:49:25 +0600

    update

--- src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
+++ src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
@@ -1,6 +1,7 @@
 package com.amigoscode.testing.utils;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,5 +27,19 @@ class PhoneNumberValidatorTest {
 
         // Then
         assertThat(isValid).isTrue();
+    }
+
+
+    @Test
+    @DisplayName("Should fail when does not start with +")
+    void itShouldValidatePhoneNumberWhenDoesNotStartWithPlus() {
+        // Given
+        String phoneNumber = "4470000000000";
+
+        // When
+        boolean isValid = underTest.test(phoneNumber);
+
+        // Then
+        assertThat(isValid).isFalse();
     }
 }
